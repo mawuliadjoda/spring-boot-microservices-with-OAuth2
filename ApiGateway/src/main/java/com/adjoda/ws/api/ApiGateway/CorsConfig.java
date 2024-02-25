a525cd2061578fe7e99d383d8dcbd4b06fb0b667
Mawuli ADJODA <koffimawulli.adjoda@gmail.com>
2024-02-25 10:43:34 +0100

    fix

--- ApiGateway/src/main/java/com/adjoda/ws/api/ApiGateway/CorsConfig.java
+++ ApiGateway/src/main/java/com/adjoda/ws/api/ApiGateway/CorsConfig.java
@@ -10,8 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Configuration
-public class CorsConfig extends
-        org.springframework.web.cors.CorsConfiguration {
+public class CorsConfig extends CorsConfiguration {
 
     @Bean
     public CorsWebFilter corsWebFilter() {
@@ -19,11 +18,12 @@ public class CorsConfig extends
         final CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.setAllowedOrigins(List.of("*"));
         corsConfig.setMaxAge(3600L);
-        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST","PUT", "DELETE"));
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         corsConfig.addAllowedHeader("Authorization");
 
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfig);
 
         return new CorsWebFilter(source);
-    }}
+    }
+}
